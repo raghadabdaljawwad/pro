98f8f6ef2c0d75d1c1bf89db83f56a9e630c37ac
raghadabdaljawwad@gmail.com <raghadabdaljawwad@gmail.com>
2023-12-11 20:40:49 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from'react';
+import 'firebase/firestore';
+import RelatedVideos from './auth/RelatedVideos';
+
+function App() {
+  return (
+    <div className="App">
+     <RelatedVideos/>
+    </div>
+  );
+}
+
+export default App;
